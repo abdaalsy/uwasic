718e3798ca807f63cce0312946b5f32bf5e7a2ca
abdaalsy <abdaalsy@gmail.com>
2026-03-23 16:35:19 -0400

    surface normals

--- rt_weekend/vec3.h
+++ rt_weekend/vec3.h
@@ -3,33 +3,31 @@
 #include <iostream>
 
 class vec3 {
-    public:
-        double e[3];
-        
-        // constructor shorthand
-        vec3() : e{0,0,0} {}
-        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+  public:
+    double e[3];
 
-        double x() const { return e[0]; }
-        double y() const { return e[1]; }
-        double z() const { return e[2]; }
-        
-        // negation, not subtracting
-        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-        double operator[](int i) const { return e[i]; }
-        double& operator[](int i) const { return e[i]; } // can bind to something??
-        
-    vec3& operator+=(const vec3& other) {
-        e[0] += other.e[0];
-        e[1] += other.e[1];
-        e[2] += other.e[2];
+    vec3() : e{0,0,0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double& operator[](int i) { return e[i]; }
+
+    vec3& operator+=(const vec3& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
         return *this;
     }
 
-    vec3& operator*=(double scalar) {
-        e[0] *= scalar;
-        e[1] *= scalar;
-        e[2] *= scalar;
+    vec3& operator*=(double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
         return *this;
     }
 
@@ -48,6 +46,7 @@ class vec3 {
 
 // point3 is just an alias for vec3, but useful for geometric clarity in the code.
 using point3 = vec3;
+
 
 // Vector Utility Functions
 
@@ -94,4 +93,3 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
 }
-
