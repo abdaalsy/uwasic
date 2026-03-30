116293845cf86bd96c876d0e33c36f4eb335ac65
abdaalsy <abdaalsy@gmail.com>
2026-03-30 00:08:00 -0400

    commit

--- rt_weekend/interval.h
+++ rt_weekend/interval.h
@@ -20,6 +20,12 @@ public:
         return min < x && x < max; // same thing as contains except x cannot be at the boundaries
     }
 
+    double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
+    }
+
     static const interval empty, universe;
 };
 
--- rt_weekend/rtweekend.h
+++ rt_weekend/rtweekend.h
@@ -3,6 +3,7 @@
 // this file will get included at the top of our main.cpp file so we don't need to write so many include statements which is kinda ugly
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -21,6 +22,16 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
 }
 
 // Common Headers
